4115e2209840f2b29aa8ccfe7eb1dcbf995473c8
Mouk Makara <makaramouk@gmail.com>
2025-01-10 19:45:01 +0700

    1. configure router, 2. Home page 3.About us 4.Bestsller 5. pricing 6.Shop

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import AboutUs from "./pages/about-us/About.jsx";
+import Layout from "./components/layout/Layout.jsx";
+import Pricing from "./pages/pricing/Pricing.jsx";
+import Products from "./pages/best-sellers/Products.jsx";
+import Shop from "./pages/shop/Shop.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // errorElement: <ErrorPage />, // Uncomment and define ErrorPage if you want to handle errors
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "home", 
+        element: <Home />,
+      },
+      {
+        path: "about-us",
+        element: <AboutUs />,
+      },
+      {
+        path: "pricing",
+        element: <Pricing/>
+      },
+      {
+        path: "best-sellers",
+        element: <Products />,
+      },
+      {
+        path: "shop",
+        element: <Shop/>
+      }
+      
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
